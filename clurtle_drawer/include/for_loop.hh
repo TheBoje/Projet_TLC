237e8d00867ffd1d54e135840dede2f09e6d37b4
Altimors <vincent.commin2000@orange.fr>
2022-01-05 23:04:24 -0500

    added for loop

--- clurtle_drawer/include/for_loop.hh
+++ clurtle_drawer/include/for_loop.hh
@@ -1,12 +1,23 @@
 #ifndef FOR_LOOP_HH
 #define FOR_LOOP_HH
 
+#include <string>
+
 #include "instr.hh"
 
 namespace clurtle {
     class for_loop : public instr {
-    
-    
+        std::string _var;
+        instr *     _body;
+
+    public:
+        for_loop(std::string var, instr * body);
+        ~for_loop();
+
+        inline std::string get_var() const { return _var; }
+        inline instr * get_body() const { return _body; }
+
+        void visit(visitor & v) const;    
     };
 }
 
