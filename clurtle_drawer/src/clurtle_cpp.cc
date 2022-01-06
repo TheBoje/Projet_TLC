ee1408f44e4a31815e944df8afc385c13dc4091b
TheBoJeu <TheBoje@users.noreply.github.com>
2022-01-05 23:04:53 -0500

    fix visit() signature fked up in some places

--- clurtle_drawer/src/clurtle_cpp.cc
+++ clurtle_drawer/src/clurtle_cpp.cc
@@ -7,6 +7,7 @@
 #include "rectangle.hh"
 #include "conditional.hh"
 #include "while_loop.hh"
+#include "constant.hh"
 
 namespace clurtle {
     clurtle_cpp::clurtle_cpp(std::string filename) : _pen_is_up(0), _pos_x(0), _pos_y(0), _rot(0) {
@@ -20,65 +21,66 @@ namespace clurtle {
             exit(EXIT_FAILURE);
         }
         
-        _color = {0, 0, 0};
+        // TODO: FREE ME PLEASE !!!
+        _color = {new constant(0), new constant(0), new constant(0)};
     }
 
     void clurtle_cpp::visit_up(const up * u) {
         _pen_is_up = true;
-        std::cout << "PEN_UP;" << std::endl;
+        std::cout << "PEN_UP();" << std::endl;
     }
 
     void clurtle_cpp::visit_down(const down * d) {
         _pen_is_up = false;
-        std::cout << "PEN_DOWN;" << std::endl;
+        std::cout << "PEN_DOWN();" << std::endl;
     }
 
     void clurtle_cpp::visit_change_color(const change_color * cc) {
         std::cout << "COLOR = {";
         _color = cc->get_color();
-        _color[0]->visit(this);
+        _color[0]->visit(*this);
         std::cout << ", ";
-        _color[1]->visit(this);
+        _color[1]->visit(*this);
         std::cout << ", ";
-        _color[2]->visit(this);
+        _color[2]->visit(*this);
         std::cout << "};" << std::endl; 
     }
 
     void clurtle_cpp::visit_forward(const forward * f) {
         std::cout << "FORWARD(";
-        f->get_amount()->visit(this);
+        f->get_amount()->visit(*this);
         std::cout << ");" << std::endl;
     }
 
     void clurtle_cpp::visit_rotate(const rotate * r) {
         std::cout << "ROTATE(";
-        r->get_amount()->visit(this);
+        r->get_amount()->visit(*this);
         std::cout << ");" << std::endl;
     }
 
     void clurtle_cpp::visit_line(const line * l) {
         std::cout << "LINE(";
-        l->get_length()->visit(this);
+        l->get_length()->visit(*this);
         std::cout << ");" << std::endl;
     }
 
     void clurtle_cpp::visit_rectangle(const rectangle * r) {
         std::cout << "RECTANGLE(";
-        r->get_length()->visit(this);
+        r->get_length()->visit(*this);
         std::cout << ", ";
-        r->get_width()->visit(this);
+        r->get_width()->visit(*this);
         std::cout << ");" << std::endl;
     }
 
     void clurtle_cpp::visit_conditional(const conditional * c) {
         std::cout << "IF (";
-        c->get_cond()->visit(this);
+        c->get_cond()->visit(*this);
         std::cout << ") {" << std::endl;
-        c->get_cons()->visit(this);
+        c->get_cons()->visit(*this);
         std::cout << "}";
         if (c->get_alt() != nullptr) {
             std::cout << "ELSE {" << std::endl;
-            c->get_alt()->visit(this);
+            c->get_alt()->visit(*this);
             std::cout << "}";
         }
         std::cout << std::endl;
@@ -86,9 +88,9 @@ namespace clurtle {
 
     void clurtle_cpp::visit_while_loop(const while_loop * wl) {
         std::cout << "WHILE (";
-        wl->get_cond()->visit(this);
+        wl->get_cond()->visit(*this);
         std::cout << ") {" << std::endl;;
-        wl->get_cons()->visit(this);
+        wl->get_body()->visit(*this);
         std::cout << "}" << std::endl;
     }
 
